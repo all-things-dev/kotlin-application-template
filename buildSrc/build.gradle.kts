0138afed72f0c135e8afaf965c96b9bf02fda511
All Things Dev <all-things-dev@users.noreply.github.com>
2023-08-25 19:19:55 +0530

    Upgraded to JDK 26

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -37,8 +37,8 @@ kotlin {
 dependencies {
 
 	// Kotlin
-	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.20")
-	implementation("org.jetbrains.kotlin:kotlin-allopen:2.3.20")
+	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0-Beta1")
+	implementation("org.jetbrains.kotlin:kotlin-allopen:2.4.0-Beta1")
 
 	// Spring Boot
 	implementation("org.springframework.boot:spring-boot-gradle-plugin:4.0.5")
