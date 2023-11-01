b2dc9cb219a28aee67051bd3dd882db676d7d626
All Things Dev <all-things-dev@users.noreply.github.com>
2023-11-01 15:00:39 +0530

    Updated Kotlin to v1.9.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -37,8 +37,8 @@ kotlin {
 dependencies {
 
 	// Kotlin
-	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20-RC2")
-	implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.20-RC2")
+	implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+	implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.20")
 
 	// Spring Boot
 	implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.4")
